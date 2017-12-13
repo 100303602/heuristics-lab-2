fd1ddebf662a64ea81d576e191019bfddb5d5468
GuillermoEscobero <a0346060@lab.inf.uc3m.es>
2017-12-13 17:14:38 +0100

    no cambios loco?

---
+++ code/part-1/AStar.py
@@ -0,0 +1,15 @@
+class car (posX, posY, cat, order):
+
+    def __init__(self, posX, posY, cat, order):
+        self.posX = posX
+        sefl.posY = posY
+        self.cat = cat
+        self.order = order
+        switch 
+        self.hValue = 
+
+def aStar(start, goal):
+    closedSet = {};
+    openSet = {start};
+
+    
